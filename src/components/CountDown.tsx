a294f94539d262be47f9e7f6002fcf6b1b395f70
abubakar167 <abubakarakram167@gmail.com>
2024-09-16 22:55:32 +0500

    Fix timer display to user.

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -13,39 +13,37 @@
 
 // export default CountDown
 
-
 // WITHOUT A LIBRARY
-"use client"
+"use client";
 import React, { useState, useEffect } from "react";
 
 const CountDown = () => {
-  
-  let difference = +new Date(`10/10/2023`) - +new Date();
-  const [delay, setDelay] = useState(difference);
+	let difference = +new Date(`11/11/2024`) - +new Date();
+	const [delay, setDelay] = useState(difference);
 
-  const d = Math.floor(difference / (1000 * 60 * 60 * 24));
-  const h = Math.floor((difference / (1000 * 60 * 60)) % 24);
-  const m = Math.floor((difference / 1000 / 60) % 60);
-  const s = Math.floor((difference / 1000) % 60);
+	const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+	const h = Math.floor((difference / (1000 * 60 * 60)) % 24);
+	const m = Math.floor((difference / 1000 / 60) % 60);
+	const s = Math.floor((difference / 1000) % 60);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setDelay(delay - 1);
-    }, 1000);
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setDelay(delay - 1);
+		}, 1000);
 
-    if (delay === 0) {
-      clearInterval(timer);
-    }
+		if (delay === 0) {
+			clearInterval(timer);
+		}
 
-    return () => {
-      clearInterval(timer);
-    };
-  });
-  return (
-    <span className="font-bold text-5xl text-yellow-300">
-      {d}:{h}:{m}:{s}
-    </span>
-  );
+		return () => {
+			clearInterval(timer);
+		};
+	});
+	return (
+		<span className="font-bold text-5xl text-yellow-300">
+			{d}:{h}:{m}:{s}
+		</span>
+	);
 };
 
 export default CountDown;
